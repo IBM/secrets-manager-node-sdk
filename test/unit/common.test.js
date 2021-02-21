3b5b1bdecda4423ee77f3f5ccb451cde4832047a
Avi Davidov <davidov@il.ibm.com>
2021-02-21 16:46:10 +0200

    fix test

--- test/unit/common.test.js
+++ test/unit/common.test.js
@@ -23,7 +23,7 @@ describe('Tests of Common Library', () => {
     test('should return correct User-Agent header', () => {
       const headers = getSdkHeaders('service1', 'v1', 'operation1');
       expect(headers).not.toEqual(null);
-      expect(headers['User-Agent']).toMatch(/^secrets-manager-sdk\/.*/);
+      expect(headers['User-Agent']).toMatch(/^secrets-manager-nodejs-sdk\/.*/);
     });
   });
 });
